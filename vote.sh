4a52a4e657d0b0085d23308d99ddaee8c9513dbf
Eino Virtanen <eino.virtanen@kapsi.fi>
2015-08-07 03:21:35 +0300

    Finding out the tor relay's ip

--- vote.sh
+++ vote.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo Hello World
+echo "Getting ip from ipchicken.com"
+IP="$(curl --socks5-hostname 127.0.0.1:9050 http://www.ipchicken.com | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
+echo "Our ip current ip is ${IP}"
+
